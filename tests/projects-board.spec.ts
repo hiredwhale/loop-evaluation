b7dc9860cb0d27595a763b2b467aca832c6a70b0
Jonathan Frederick <jon.ed.fred@gmail.com>
2025-10-14 21:09:01 -0500

    Fix typo

--- tests/projects-board.spec.ts
+++ tests/projects-board.spec.ts
@@ -9,7 +9,7 @@ const loginInfo = {
 };
 
 for (const { name, project, column, tags } of taskList) {
-  test(`Verify ${name} appear correctly on projects board`, async ({ page }) => {
+  test(`Verify ${name} appears correctly on projects board`, async ({ page }) => {
     const login = new LoginPage(page);
     const projects = new ProjectsPage(page);
 
